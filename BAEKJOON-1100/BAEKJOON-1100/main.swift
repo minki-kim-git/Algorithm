92de5003703a076de3718aa56612bc21484ceea3
minki-kim-git <rlaalsrl1227@gmail.com>
2022-02-25 23:59:38 +0900

    BAEKJOON-1100

---
+++ BAEKJOON-1100/BAEKJOON-1100/main.swift
@@ -0,0 +1,36 @@
+//
+//  main.swift
+//  BAEKJOON-1100
+//
+//  Created by 김민기 on 2022/02/25.
+//
+
+import Foundation
+var a = ["F","","F","","F","","F",""]
+var b = ["","F","","F","","F","","F"]
+var count = 0
+for i in 0...7{
+    let input = readLine()!.map{String($0)}
+    
+    if i % 2 == 0 {
+        fir(str:input)
+    } else {
+        sec(str:input)
+    }
+}
+
+func fir(str :[String]){
+    for i in 0...7{
+        if b[i] == str[i] {
+            count += 1
+        }
+    }
+}
+func sec(str :[String]){
+    for i in 0...7{
+        if a[i] == str[i] {
+            count += 1
+        }
+    }
+}
+print(count)
